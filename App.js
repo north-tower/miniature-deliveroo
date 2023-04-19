c5390c570a35e01803107039f4be4eba4fd09fea
north-tower <muneshmyk@gmail.com>
2023-04-19 16:38:05 +0300

    Initialized

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-react-native-classnames"
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={tw`text-red-500`}>Open up App.js  app!</Text>
       <StatusBar style="auto" />
     </View>
   );
